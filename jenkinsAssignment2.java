23ae4454970e4508eb67b99159077627c97ac6fa
Ayush <144894664+ayush2333@users.noreply.github.com>
2024-10-24 13:42:10 +0530

    Update v2 jenkinsAssignment2.java

--- jenkinsAssignment2.java
+++ jenkinsAssignment2.java
@@ -1,7 +1,7 @@
 public class jenkinsAssignment2 {
     public static void main(String[] args) {
         
-        System.out.println("Hey thereee");
+        System.out.println("Hey thereee v2");
 
     }
 }
